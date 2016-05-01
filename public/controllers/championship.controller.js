6f176e2b73f6116bc7dc2619ff7f048120501463
Fauricio Rojas <fauri-94@hotmail.com>
2016-05-01 01:30:26 -0600

    Championship, files example and about me complete

--- public/controllers/championship.controller.js
+++ public/controllers/championship.controller.js
@@ -5,22 +5,56 @@
 		.module('hakemboApp')
 		.controller('ChampionshipController', ChampionshipController);
 
-	ChampionshipController.$inject = ['$http', 'ChampionshipFactory'];
-	function ChampionshipController($http, ChampionshipFactory) {
+	ChampionshipController.$inject = ['$http', '$timeout', 'ChampionshipFactory'];
+	function ChampionshipController($http, $timeout, ChampionshipFactory) {
 		var vm = this;
+		vm.content = '';
 		vm.uploadFile = uploadFile;
+		vm.championship = championship;
 
-		function uploadFile() {
-			console.log(document.getElementById("file").value);
+		function readBlob(opt_startByte, opt_stopByte) {
+    		var files = document.getElementById('file').files;
 
-			/*$http.get(document.getElementById("file").value)
-			.success(function(response) {
-				console.log(response);
-			})
-			.error(function(err) {
-				console.log(err);
-			});*/
-			
-		}
+    		if (!files.length) {
+      			return;
+    		}
+
+		    var file = files[0];
+		    var start = parseInt(opt_startByte) || 0;
+		    var stop = parseInt(opt_stopByte) || file.size - 1;
+
+		    var reader = new FileReader();
+
+    		// If we use onloadend, we need to check the readyState.
+    		reader.onloadend = function(evt) {
+      			if (evt.target.readyState == FileReader.DONE) {
+        			document.getElementById('content').textContent = evt.target.result;
+      			}
+    		};
+
+    		var blob = file.slice(start, stop + 1);
+    		reader.readAsBinaryString(blob);
+  		}
+
+		function uploadFile(evt) {
+			var startByte = evt.target.getAttribute('data-startbyte');
+  			var endByte = evt.target.getAttribute('data-endbyte');
+  			readBlob(startByte, endByte);
+  		}
+
+  		function championship() {
+  			var content = document.getElementById('content').textContent;
+
+  			ChampionshipFactory.championship(content)
+  			.then(function(response) {
+  				vm.winner = response[0];
+  				vm.strategy = response[1];
+
+  				$timeout(function() {
+  					vm.winner = '';
+  					document.getElementById('content').textContent = '';
+  				}, 10000);
+  			});
+  		}
 	}
 })();
--- public/factories/championship.factory.js
+++ public/factories/championship.factory.js
@@ -8,10 +8,31 @@
 	ChampionshipFactory.$inject = ['$http', '$q'];
 	function ChampionshipFactory($http, $q) {
 		var factory = {
-			
+			championship: championship
 		};
 
 		return factory;
+
+		function championship(content) {
+			var defered = $q.defer(),
+				data = {
+					data: content
+				};
+
+			$http({
+				method: 'POST',
+				url: 'api/championship/new',
+				data: data
+			})
+			.success(function(response) {
+				defered.resolve(response.winner);
+			})
+			.error(function(err) {
+				defered.reject(err);
+			});
+
+			return defered.promise;
+		}
 	}
 
 })();
